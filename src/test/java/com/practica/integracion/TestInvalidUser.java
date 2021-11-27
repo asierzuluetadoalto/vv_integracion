37ad40235a844d6f4faa57c1318278e3a0a78431
jlrojo31 <93144609+jlrojo31@users.noreply.github.com>
2021-11-27 12:31:17 +0100

    primera versión

--- src/test/java/com/practica/integracion/TestInvalidUser.java
+++ src/test/java/com/practica/integracion/TestInvalidUser.java
@@ -1,11 +1,82 @@
 package com.practica.integracion;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.practica.integracion.DAO.AuthDAO;
+import com.practica.integracion.DAO.GenericDAO;
+import com.practica.integracion.DAO.User;
+import com.practica.integracion.manager.SystemManager;
+import com.practica.integracion.manager.SystemManagerException;
 
 @ExtendWith(MockitoExtension.class)
 public class TestInvalidUser {
-	/**
-	 * RELLENAR POR EL ALUMNO
-	 */
+	@Mock
+	private AuthDAO authDao;
+	@Mock
+	private GenericDAO genericDao;
+	
+	private User usuario;
+	private Object remote;
+	private SystemManager systemManager;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		usuario = new User("10","nombre","apellido","dir",new ArrayList<Object>());
+		remote = new Object();
+		
+		systemManager = new SystemManager(authDao,genericDao);
+	}
+	
+	@Test
+	public void testAddRemoteSystem() throws OperationNotSupportedException, SystemManagerException{		
+		when(authDao.getAuthData("10")).thenReturn(null);
+		when(genericDao.updateSomeData(null, remote)).thenReturn(false); //quizá la excepción
+		
+		assertThrows(SystemManagerException.class, () -> {systemManager.addRemoteSystem("10", remote);});
+		verify(authDao, times(1)).getAuthData("10");
+	}
+	
+	@Test
+	public void testStartRemoteSystem() throws OperationNotSupportedException, SystemManagerException{		
+		when(authDao.getAuthData("10")).thenReturn(null);
+		when(genericDao.getSomeData(Mockito.anyObject(),Mockito.anyString())).thenThrow(OperationNotSupportedException.class);
+		
+		assertThrows(SystemManagerException.class, () -> {systemManager.startRemoteSystem("10", "remId");});
+		verify(authDao, times(1)).getAuthData("10");
+	}
+	
+	@Test
+	public void testStopRemoteSystem() throws OperationNotSupportedException, SystemManagerException{
+		when(authDao.getAuthData("10")).thenReturn(null);
+		when(genericDao.getSomeData(Mockito.anyObject(),Mockito.anyString())).thenThrow(OperationNotSupportedException.class);
+		
+		assertThrows(SystemManagerException.class, () -> {systemManager.stopRemoteSystem("10", "1");});
+		verify(authDao, times(1)).getAuthData("10");
+	}
+	
+	@Test
+	public void testDeleteRemoteSystem() throws OperationNotSupportedException, SystemManagerException{
+		when(authDao.getAuthData("10")).thenReturn(null);
+		when(genericDao.deleteSomeData(Mockito.anyObject(), Mockito.anyString())).thenReturn(false); //quizá la excepción
+		
+		assertThrows(SystemManagerException.class, () -> {systemManager.deleteRemoteSystem("10", "remId");});
+		verify(authDao, times(1)).getAuthData("10");
+	}
 }
--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -1,13 +1,101 @@
 package com.practica.integracion;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.practica.integracion.DAO.AuthDAO;
+import com.practica.integracion.DAO.GenericDAO;
+import com.practica.integracion.DAO.User;
+import com.practica.integracion.manager.SystemManager;
+import com.practica.integracion.manager.SystemManagerException;
 
 @ExtendWith(MockitoExtension.class)
 public class TestValidUser {
 
-	/**
-	 * RELLENAR POR EL ALUMNO
-	 */
+	@Mock
+	private AuthDAO authDao;
+	@Mock
+	private GenericDAO genericDao;
+	
+	private User usuario;
+	private Object remote;
+	private SystemManager systemManager;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		usuario = new User("10","nombre","apellido","dir",new ArrayList<Object>());
+		remote = new Object();
+		
+		systemManager = new SystemManager(authDao,genericDao);
+	}
+	
+	
+	@Test
+	public void testAddRemoteSystem() throws OperationNotSupportedException, SystemManagerException{		
+		when(authDao.getAuthData("10")).thenReturn(usuario);
+		when(genericDao.updateSomeData(usuario, remote)).thenReturn(true);
+		InOrder inOrder = Mockito.inOrder(authDao,genericDao);
+		
+		systemManager.addRemoteSystem("10", remote);
+		
+		verify(authDao, times(1)).getAuthData("10");	
+		inOrder.verify(authDao).getAuthData("10");
+		inOrder.verify(genericDao).updateSomeData(usuario, remote);
+	}
+	
+	@Test
+	public void testStartRemoteSystem() throws OperationNotSupportedException, SystemManagerException{		
+		when(authDao.getAuthData("10")).thenReturn(usuario);
+		when(genericDao.getSomeData(usuario,"where id=remId")).thenReturn(new HashSet());
+		InOrder inOrder = Mockito.inOrder(authDao,genericDao);
+		
+		systemManager.startRemoteSystem("10", "remId");
+		
+		verify(authDao, times(1)).getAuthData("10");
+		inOrder.verify(authDao).getAuthData("10");
+		inOrder.verify(genericDao).getSomeData(usuario, "where id=remId");
+	}
+	
+	@Test
+	public void testStopRemoteSystem() throws OperationNotSupportedException, SystemManagerException{
+		when(authDao.getAuthData("10")).thenReturn(usuario);
+		when(genericDao.getSomeData(usuario,"where id=remId")).thenReturn(new HashSet());
+		InOrder inOrder = Mockito.inOrder(authDao,genericDao);
+		
+		systemManager.stopRemoteSystem("10", "remId");
+		
+		verify(authDao, times(1)).getAuthData("10");
+		inOrder.verify(authDao).getAuthData("10");
+		inOrder.verify(genericDao).getSomeData(usuario, "where id=remId");
+	}
+	
+	@Test
+	public void testDeleteRemoteSystem() throws OperationNotSupportedException, SystemManagerException{
+		when(authDao.getAuthData("10")).thenReturn(usuario);
+		when(genericDao.deleteSomeData(Mockito.any(User.class), Mockito.anyString())).thenReturn(true);
+		
+		systemManager.deleteRemoteSystem("10", "remId");
+		
+		verify(authDao, times(1)).getAuthData("10");	
+		InOrder inOrder = Mockito.inOrder(authDao,genericDao);
+		inOrder.verify(authDao).getAuthData("10");
+		inOrder.verify(genericDao).deleteSomeData(Mockito.any(User.class), Mockito.anyString());
+		
+	}
+		
 
 }
